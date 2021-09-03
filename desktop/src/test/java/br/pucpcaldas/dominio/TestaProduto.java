efac70dd51dcc1e8584398b45cd9e309d9f72eda
geovaniDarcie <geovani_darcie@hotmail.com>
2021-09-03 20:15:55 -0300

    Classe de domínio Produto

---
+++ desktop/src/test/java/br/pucpcaldas/dominio/TestaProduto.java
@@ -0,0 +1,22 @@
+package br.pucpcaldas.dominio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class TestaProduto {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testeConstrutor_DeveCriarUmProdutoValido() {
+        Produto caneta = new Produto(1, "caneta", 1.50);
+
+        assertEquals(1, caneta.getCodigo());
+        assertEquals("caneta", caneta.getNome());
+        assertEquals(1.50, caneta.getPreco());
+    }
+}
